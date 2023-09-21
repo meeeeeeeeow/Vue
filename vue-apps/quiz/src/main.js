51d6a49da5951e6765ab51964442c91386315652
meow <ivy8792@gmail.com>
2023-09-21 14:16:23 +0800

    Create quiz demo

---
+++ vue-apps/quiz/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
+
+Vue.use(BootstrapVue)
+
+createApp(App).mount('#app')
